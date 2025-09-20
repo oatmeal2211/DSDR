e2fa9001b0bc969665192668f2b6ae9bc9b1feaa
oatmeal2211 <maxyjared@gmail.com>
2025-09-21 04:33:55 +0800

    another change

--- advanced_airline_dashboard.py
+++ advanced_airline_dashboard.py
@@ -307,7 +307,7 @@ def analyze_happiness_drivers(df, service_cols):
         fig.update_layout(title="Service Ratings: Happy vs Frustrated Passengers",
                          xaxis_title="Service Aspect", yaxis_title="Average Rating",
                          barmode='group', height=400)
-        fig.update_xaxis(tickangle=45)
+        fig.update_xaxes(tickangle=45)
         st.plotly_chart(fig, use_container_width=True)
         
         # Service gaps
